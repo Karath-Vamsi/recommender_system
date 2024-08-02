26b2e3f25cadfa2715c8d0d868279eef297339d1
Karath-Vamsi <ugs22047_aids.vamsi@cbit.org.in>
2024-08-03 00:11:11 +0530

    main.py run success

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 import data_pipeline as pipe
+import model as sys
 import numpy as np
+import tensorflow as tf
 
-def main():
-
-    data_pipe = pipe.Data_procure()
-
-    data_pipe.load_extract()
-    movieList, movieList_df = data_pipe.load_Movie_List_pd()
+def create_ratings(movieList):
 
     my_ratings = np.zeros(len(movieList))
 
@@ -24,3 +21,42 @@ def main():
     my_ratings[2937] = 1   
     my_ratings[793]  = 5   
     my_rated = [i for i in range(len(my_ratings)) if my_ratings[i] > 0]
+
+    return my_ratings, my_rated
+
+def main():
+
+    data_pipe = pipe.Data_procure()
+
+    data_pipe.load_extract()
+    movieList, movieList_df = data_pipe.load_movie_list()
+
+    my_ratings, my_rated = create_ratings(movieList)
+
+    Y, R = data_pipe.load_ratings()
+
+    Y = np.c_[my_ratings, Y]
+    R = np.c_[(my_ratings != 0).astype(int), R]
+
+    # Build the model
+    model = sys.Model_builder()
+    X, W, b, Ymean = model.model(Y, R, 1)
+
+    predictions = model.predict(X, W, b, Ymean)
+
+    my_predictions = predictions[:,0]
+
+    ix = tf.argsort(my_predictions, direction='DESCENDING')
+
+    for i in range(17):
+        j = ix[i]
+        if j not in my_rated:
+            print(f'Predicting rating {my_predictions[j]:0.2f} for movie {movieList[j]}')
+
+    print('\n\nOriginal vs Predicted ratings:\n')
+    for i in range(len(my_ratings)):
+        if my_ratings[i] > 0:
+            print(f'Original {my_ratings[i]}, Predicted {my_predictions[i]:0.2f} for {movieList[i]}')
+
+if __name__ == '__main__':
+    main()
